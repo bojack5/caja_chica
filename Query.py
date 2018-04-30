759656fcc7708cf729a84239ae0085fa32a5d389
ROG <administracion@borbolla-automation.com>
2018-04-29 23:05:25 -0500

    acceso a google sheets

--- Query.py
+++ Query.py
@@ -3,4 +3,8 @@ import datetime , peewee
 from Models import Proveedor , Ticket , Contacto , Movimiento , Abono
 
 
-Proveedor = Proveedor.create()
+Proveedor = Proveedor.create(nombre = "Tuvaco" , direccion = "Blvd. Jaime Benavides Pompa 648, Zona Industrial, 25220 Ramos Arizpe")
+
+Contacto = Contacto.create(nombre = "Ventas" , telefono = 4880193 , email = "tuvaco@hotmail.com" , puesto = "ventas" , proveedor = Proveedor)
+
+
---
+++ prueba.py
@@ -0,0 +1,19 @@
+import gspread 
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name('CajaChica-5a38455e3dd8.json',scope)
+
+gc = gspread.authorize(credentials)
+
+wks = gc.open('Caja Chica y Gastos').sheet1
+
+print(wks.get_all_records())
+
+wks.append_row(['linea nueva col 1','linea nueva col 2'])
+
+
+
+
+print(wks.get_all_records())
